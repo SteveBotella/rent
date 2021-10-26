744d8a6ce4ccf45e26ef601de6eda9ad4e3b0347
Steve <stevebotella@hotmail.fr>
2021-10-26 14:21:25 +0200

    feat: add image variable for product

--- src/main/java/com/rent/rent/dao/ProductDaoImpl.java
+++ src/main/java/com/rent/rent/dao/ProductDaoImpl.java
@@ -12,9 +12,9 @@ public class ProductDaoImpl implements ProductDao {
     public static List<Product> products = new ArrayList<>();
 
     static {
-        products.add(new Product(1, new String("Renault Master"), 50));
-        products.add(new Product(10, new String("Fiat Ducato"), 60));
-        products.add(new Product(30, new String("Mercedes Sprinter"), 100));
+        products.add(new Product(1, new String("Master"), new String("Renault"), new String("yellow"), 50, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMjs4cV87nu22LM9klvK49P60pUZMoYbgt7ezrByc8ad6PtidGbn9GJKvN1cajiJJeDg&usqp=CAU")));
+        products.add(new Product(10, new String("Ducato"), new String("Fiat"), new String("red"), 60, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU3IIya8F6A0Vd_hHzWyEim2NkHH7PhTsqG1JW5RfuBt__szBghbAeQBPW0wbSPrOXoVU&usqp=CAU")));
+        products.add(new Product(30, new String("Sprinter"), new String("Mercedes"), new String("light grey"), 100, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBTueSHaoXL5_LtVSPLNrA8mZXGcQezMAV99yXBUXrdNWqrkQD4LOI5CjGJrhqyeDUqO0&usqp=CAU")));
     }
 
     @Override
--- src/main/java/com/rent/rent/model/Product.java
+++ src/main/java/com/rent/rent/model/Product.java
@@ -4,36 +4,66 @@ public class Product {
 
     private int id;
     private String model;
+    private String brand;
+    private String color;
     private int price;
+    private String image;
 
-    public Product(int id, String model, int price) {
+    public Product(int id, String model, String brand, String color, int price, String image) {
         this.id = id;
         this.model = model;
+        this.brand = brand;
+        this.color = color;
         this.price = price;
+        this.image = image;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getModel() {
-        return model;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getModel() {
+        return model;
     }
 
     public void setModel(String model) {
         this.model = model;
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     @Override
@@ -41,6 +71,8 @@ public class Product {
         return "Product{" +
                 "id=" + id +
                 ", model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
                 ", price=" + price +
                 '}';
     }
