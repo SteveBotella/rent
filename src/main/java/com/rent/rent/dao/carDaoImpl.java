5886bc8eadb5e1d929731c15a64e375329b5415a
Steve <stevebotella@hotmail.fr>
2021-10-28 11:25:49 +0200

    fix: Change static methode with a constructor logic

--- src/main/java/com/rent/rent/dao/carDaoImpl.java
+++ src/main/java/com/rent/rent/dao/carDaoImpl.java
@@ -9,12 +9,20 @@ import java.util.List;
 @Repository
 public class carDaoImpl implements carDao {
 
-    public static List<Car> cars = new ArrayList<>();
+    public List<Car> cars = new ArrayList<>();
 
-    static {
+    public carDaoImpl(){
         cars.add(new Car(1, new String("Master"), new String("Renault"), new String("yellow"), 50, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMjs4cV87nu22LM9klvK49P60pUZMoYbgt7ezrByc8ad6PtidGbn9GJKvN1cajiJJeDg&usqp=CAU")));
         cars.add(new Car(10, new String("Ducato"), new String("Fiat"), new String("red"), 60, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU3IIya8F6A0Vd_hHzWyEim2NkHH7PhTsqG1JW5RfuBt__szBghbAeQBPW0wbSPrOXoVU&usqp=CAU")));
         cars.add(new Car(30, new String("Sprinter"), new String("Mercedes"), new String("light grey"), 100, new String("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBTueSHaoXL5_LtVSPLNrA8mZXGcQezMAV99yXBUXrdNWqrkQD4LOI5CjGJrhqyeDUqO0&usqp=CAU")));
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
     @Override
