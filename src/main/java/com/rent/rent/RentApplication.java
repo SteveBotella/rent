a5e72f863eea38092d56c896251148f536f6da23
Steve <stevebotella@hotmail.fr>
2021-10-28 16:03:51 +0200

    feat: install swagger

--- src/main/java/com/rent/rent/RentApplication.java
+++ src/main/java/com/rent/rent/RentApplication.java
@@ -2,8 +2,10 @@ package com.rent.rent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RentApplication {
 
 	public static void main(String[] args) {
